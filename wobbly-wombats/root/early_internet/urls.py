101d98943356ae75dde67ec0032c0cc998c59daa
Ceres <ddruva.boomer@gmail.com>
2020-08-03 21:44:23 +0530

    added registration and login url

--- wobbly-wombats/root/early_internet/urls.py
+++ wobbly-wombats/root/early_internet/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quiz/', include('quiz.urls'))
+    path('quiz/', include('quiz.urls')),
+    path('accounts/', include('accounts.urls', 'accounts')),
+    path('', homepage)
 ]
