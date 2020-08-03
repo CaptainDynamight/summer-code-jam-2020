23606c0918a6b09bd1d9bc712bc4c64cf7312b10
Ceres <ddruva.boomer@gmail.com>
2020-08-03 21:42:57 +0530

    user registration and login

--- wobbly-wombats/root/accounts/models.py
+++ wobbly-wombats/root/accounts/models.py
@@ -2,10 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class User(AbstractUser):
+class CustomUser(AbstractUser):
     """
     Username and password are required. Other fields are optional.
     """
+    user = models.CharField(max_length=40, unique=True)
 
     image = models.ImageField(
         upload_to='images/users',
@@ -13,12 +14,7 @@ class User(AbstractUser):
         blank=True
     )
     points = models.PositiveIntegerField(default=0, verbose_name="Points gained")
+    USERNAME_FIELD = 'user'
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
-
-
-
-
-
-
--- wobbly-wombats/root/accounts/views.py
+++ wobbly-wombats/root/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserCreationForm
 
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
